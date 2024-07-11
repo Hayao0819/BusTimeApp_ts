291cff2a6f9c108b5aad53734f81d743a3e62a98
hayao <hayao@fascode.net>
2024-07-11 10:12:19 +0900

    Prepare for PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const PWAConfig: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  injectRegister: "auto",
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), VitePWA(PWAConfig)],
   test: {
     globals: true,
     environment: "happy-dom",
